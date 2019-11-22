0a43901cc2ae23f0344811017c472882e229166e
Demetrious Robinson <drobinson@pivotal.io>
2019-11-21 17:06:48 -0800

    backend query

--- src/main/kotlin/com/kotlingraphqlexample/kotlingraphqlexample/PersonQueryResolver.kt
+++ src/main/kotlin/com/kotlingraphqlexample/kotlingraphqlexample/PersonQueryResolver.kt
@@ -3,10 +3,13 @@ package com.kotlingraphqlexample.kotlingraphqlexample
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import org.springframework.stereotype.Component
 
+data class AllPeople (val people: List<Person>)
+
 //just an example for using GraphQLQueryResolver, which is used to retrieve the root fields
 @Component
 class PersonQueryResolver (private val personDao: PersonDao): GraphQLQueryResolver {
     fun getPersonById(id: String) = personDao.getPersonById(id)
     fun getPersonByName(name: String) = personDao.getPersonByName(name)
     fun getAllPeople(offset: Int) = personDao.getAllPeople(offset)
+    fun getEveryone(offset: Int): AllPeople = AllPeople(personDao.getAllPeople(offset))
 }
