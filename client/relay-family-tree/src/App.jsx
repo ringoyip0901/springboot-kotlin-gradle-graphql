e45d1eb5d72db4a73f5338858920ca05eb71a37c
ringoyip0901 <ringoyip0901@gmail.com>
2019-11-22 23:32:14 -0800

    wip - relay pagination

--- client/relay-family-tree/src/App.jsx
+++ client/relay-family-tree/src/App.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import {QueryRenderer} from 'react-relay';
-import PersonNameContainer from './PersonNameContainer.jsx'
-import PersonImageContainer from './PersonImageContainer.jsx'
-import ListOfPeople from './ListOfPeople'
-import VersionContainer from './VersionContainer'
-import environment from './relayEnvir.js'
+import PersonNameContainer from './PersonNameContainer.jsx';
+import PersonImageContainer from './PersonImageContainer.jsx';
+import ListOfPeople from './ListOfPeople';
+import VersionContainer from './VersionContainer';
+import environment from './relayEnvir.js';
 import logo from './logo.svg';
 import './App.css';
-import graphql from 'babel-plugin-relay/macro'
+import graphql from 'babel-plugin-relay/macro';
 import ListOfPeoplePagination from '../ListOfPeoplePagination.js';
 
-function App() {
+function App () {
   return (
-      <QueryRenderer
-        environment={environment}
-        query={graphql`
-            query AppQuery ($offset: Int!, $name: String) {
+    <QueryRenderer
+      environment={environment}
+      query={graphql`
+            query AppQuery ($offset: Int!, $name: String, $count: Int, $cursor: String) {
               version {
                 ...VersionContainer_version,
               }
@@ -23,40 +23,40 @@ function App() {
                 ...PersonNameContainer_getPersonByName,
                 ...PersonImageContainer_getPersonByName
               }
-<<<<<<< HEAD
-              getEveryone {
-                ...PeopleContainer_list
-=======
               getEveryone(offset: $offset) {
                 ...ListOfPeople_list @arguments(name: $name)
->>>>>>> 64df5aea89678a2c4519c0f8d7792857c96adba4
+                ...ListOfPeoplePagination_paginatedList @arguments(count: $count, cursor: $cursor)
               }
             }
-        `
-        }
-        variables={{
-          offset: 0,
-          name: "Prince William"
-        }}
-        render={({error, props}) => {
-          if (error) {
-            return <div>There was an error yo: {error.message}, {JSON.stringify(error)}</div>
-          } else if (props) {
-<<<<<<< HEAD
-            console.log("top props: ", props)
-=======
->>>>>>> 64df5aea89678a2c4519c0f8d7792857c96adba4
-            return <>
+        `}
+      variables={{
+        offset: 0,
+        name: 'Prince William',
+        count: 3,
+        cursor: "4"
+      }}
+      render={({error, props}) => {
+        if (error) {
+          return (
+            <div>
+              There was an error yo: {error.message}, {JSON.stringify (error)}
+            </div>
+          );
+        } else if (props) {
+          return (
+            <React.Fragment>
               {/* <VersionContainer version={props.version}/> */}
-              <ListOfPeople list={props.getEveryone}/>
+              <ListOfPeople list={props.getEveryone} />
               <ListOfPeoplePagination />
               {/* <PersonNameContainer getPersonByName={props.getPersonByName}/>
               <PersonImageContainer getPersonByName={props.getPersonByName}/> */}
-              </>
+            </React.Fragment>
+          );
         } else {
-          return <div>Loading...</div>
-        }}}
-      />
+          return <div>Loading...</div>;
+        }
+      }}
+    />
   );
 }
 
