aadadaec526e91ce1071798e25da4fbd89050493
Ringo Yip <ringo.yip.ctr@sage.northcom.mil>
2019-12-06 14:34:31 -0800

    minor refactor

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,12 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-mustache")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	runtimeOnly("com.h2database:h2")
+
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
--- src/main/kotlin/com/kotlingraphqlexample/kotlingraphqlexample/PersonQueryResolver.kt
+++ src/main/kotlin/com/kotlingraphqlexample/kotlingraphqlexample/PersonQueryResolver.kt
@@ -5,19 +5,23 @@ import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import com.coxautodev.graphql.tools.GraphQLResolver
 import org.springframework.stereotype.Component
 
-val edges = Edges(
-    mutableListOf(
-        Edge("1", PersonDao().getPersonById("1")),
-        Edge("2", PersonDao().getPersonById("2")),
-        Edge("3", PersonDao().getPersonById("3")),
-        Edge("4", PersonDao().getPersonById("4")),
-        Edge("5", PersonDao().getPersonById("5")),
-        Edge("6", PersonDao().getPersonById("6")),
-        Edge("7", PersonDao().getPersonById("7")),
-        Edge("8", PersonDao().getPersonById("8"))
-    ),
-    PageInfo()
-)
+//val originalData = PersonDao().data;
+//
+val edges = Edges(data.map { person -> Edge(person.id, PersonDao().getPersonById(person.id)) }, PageInfo());
+
+//val edges = Edges(
+//    mutableListOf(
+//        Edge("1", PersonDao().getPersonById("1")),
+//        Edge("2", PersonDao().getPersonById("2")),
+//        Edge("3", PersonDao().getPersonById("3")),
+//        Edge("4", PersonDao().getPersonById("4")),
+//        Edge("5", PersonDao().getPersonById("5")),
+//        Edge("6", PersonDao().getPersonById("6")),
+//        Edge("7", PersonDao().getPersonById("7")),
+//        Edge("8", PersonDao().getPersonById("8"))
+//    ),
+//    PageInfo()
+//)
 
 // just an example for using GraphQLQueryResolver, which is used to retrieve the root fields
 @Component
@@ -39,22 +43,12 @@ class AllPeopleQueryResolver(private val personDao: PersonDao) : GraphQLResolver
     }
 
     fun allPeople(allPeople: AllPeople, name: String?, image: String?, first: Int, after: String = "1"): Edges {
-//        fun getPersonById(id: String) = personDao.getPersonById(id)
-//        val filteredByName: List<Person> = if (name != null) allPeople.people.filter { it.name == name } else allPeople.people
-//        val filteredByImage: List<Person> = if (image != null) filteredByName.filter { it.image == image } else filteredByName
-//        val sortedById: List<Person> = if (cursor != null) filteredByImage.filter { Integer.parseInt(it.id) < Integer.parseInt(cursor) }.sortedBy { it.id } else filteredByImage
-        // val paginatedEdges: List<Person> = edges.edges.subList(after, first)
-        println("first: " + first)
-        println("cursor: " + after)
-        val listOfEdges = edges.edges;
+        val listOfEdges = Edges(data.map { person -> Edge(person.id, PersonDao().getPersonById(person.id)) }, PageInfo()).edges;
         val indexOfAfterCursor: Int = listOfEdges.indexOfFirst{ it.cursor == after}
-
-        val hasNextPage: Boolean = if (after == "7") false else true //Relay can understand whether a refetch is necessary based on this piece of information
+        val hasNextPage: Boolean = if (after == data.lastIndex.toString()) false else true //Relay can understand whether a refetch is necessary based on this piece of information
         val selectedListOfEdges = listOfEdges.slice(IntRange(indexOfAfterCursor, indexOfAfterCursor + 1))
-        println("IndexOfAfterCursor: " + indexOfAfterCursor)
-        //val selectedListOfEdges = mutableListOf<Edge>(listOfEdges[indexOfAfterCursor])
-        println(selectedListOfEdges.size)
         val resultEdges = Edges(selectedListOfEdges, PageInfo(hasNextPage = hasNextPage, endCursor = selectedListOfEdges.last().cursor))
+        println(listOfEdges.size)
         return resultEdges
     }
 }
