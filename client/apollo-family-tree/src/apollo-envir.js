9bdf2da34c8f898b3df78b14ac7ab6b57bb1ea68
ringoyip0901 <ringoyip0901@gmail.com>
2019-12-14 18:45:06 -0800

    trying to connect to heroku

--- client/apollo-family-tree/src/apollo-envir.js
+++ client/apollo-family-tree/src/apollo-envir.js
@@ -1,55 +1,46 @@
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-import ApolloClient from "apollo-client";
-import {InMemoryCache} from "apollo-cache-inmemory";
-import {gql} from "apollo-boost";
-import { SubscriptionClient } from 'subscriptions-transport-ws'
+import {split} from 'apollo-link';
+import {HttpLink} from 'apollo-link-http';
+import {WebSocketLink} from 'apollo-link-ws';
+import {getMainDefinition} from 'apollo-utilities';
+import ApolloClient from 'apollo-client';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {gql} from 'apollo-boost';
+import {SubscriptionClient} from 'subscriptions-transport-ws';
 
 // Create an http link:
-const httpLink = new HttpLink({
-    uri: 'http://localhost:8080/graphql'
+const httpLink = new HttpLink ({
+  uri: '/graphql',
 });
 
-const subscriptionClient = new SubscriptionClient('ws://localhost:8080/subscriptions', { reconnect: false });
+// const subscriptionClient = new SubscriptionClient (
+//   'ws://localhost:8080/subscriptions',
+//   {reconnect: false}
+// );
 
 // Create a WebSocket link:
 // const wsLink = new WebSocketLink(subscriptionClient)
-const wsLink = new WebSocketLink({
-    uri: "ws:localhost:8080/subscriptions"
-})
-
+const wsLink = new WebSocketLink ({
+  uri: '/subscriptions',
+});
 
 // using the ability to split links, you can send data to each link
 // depending on what kind of operation is being sent
-const link = split(
-    // split based on operation type
-    ({ query }) => {
-        const definition = getMainDefinition(query);
-        return (
-            definition.kind === 'OperationDefinition' &&
-            definition.operation === 'subscription'
-        );
-    },
-    wsLink,
-    httpLink,
+const link = split (
+  // split based on operation type
+  ({query}) => {
+    const definition = getMainDefinition (query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink
 );
 
-const client = new ApolloClient({
-    link,
-    cache: new InMemoryCache()
-})
-
-client
-.query ({
-    query: gql`
-        {
-            message
-        }
-    `,
-})
-.then (res => res['data'])
-.then (res => res);
+const client = new ApolloClient ({
+  link,
+  cache: new InMemoryCache (),
+});
 
 export default client;
